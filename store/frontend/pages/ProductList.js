1e51551a4cd41b70fb93ed3880c356b1b1fcbbd4
pekkalicious <pekkalicious34@gmail.com>
2024-06-01 17:03:45 +0200

    Create ProductList.js

---
+++ store/frontend/pages/ProductList.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/products')
+            .then(response => setProducts(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            <h2>Products</h2>
+            <div className="product-list">
+                {products.map(product => (
+                    <div key={product.id} className="product-item">
+                        <img src={product.image} alt={product.name} />
+                        <h3>{product.name}</h3>
+                        <p>{product.price}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ProductList;
